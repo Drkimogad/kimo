dedf5635679f6101123bacdc76a8bb13b0f256df
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-18 20:52:52 +0200

    Update models.js

--- models.js
+++ models.js
@@ -1,22 +1,11 @@
 // Load Text & Image Models
-let textModel, imageModel;
+let textModel, imageModel, handwritingModel;
 
 // Function to Load AI Models
 async function loadModels() {
-  try {
-    console.log("Loading models...");
-
-    // Load Text Model (Pretrained)
-    textModel = await use.load();
-    console.log("Text model loaded!");
-
-    // Load Image Model (Pretrained)
-    imageModel = await tf.loadLayersModel('/models/image-model.json');
-    console.log("Image model loaded!");
-
-  } catch (error) {
-    console.error("Error loading models:", error);
-  }
+  textModel = await use.load();
+  imageModel = await tf.loadLayersModel('/models/image_classification/model.json');
+  handwritingModel = await tf.loadLayersModel('/models/handwriting_recognition/model.json');
 }
 
 // Call Model Loader on App Start
