46c821f1753c062ebfb2f690e21da12e2f225638
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-21 16:40:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-export * from 'https://esm.sh/@tensorflow/tfjs@4.22.0/es2022/tfjs.mjs';
+import * as tf from 'https://esm.sh/@tensorflow/tfjs@4.22.0/es2022/tfjs.mjs';
 import 'https://esm.sh/@tensorflow/tfjs-backend-cpu@4.22.0/es2022/tfjs-backend-cpu.mjs';
 import 'https://esm.sh/@tensorflow/tfjs-backend-webgl@4.22.0/es2022/tfjs-backend-webgl.mjs';
 import 'https://esm.sh/@tensorflow/tfjs-converter@4.22.0/es2022/tfjs-converter.mjs';
@@ -7,3 +7,5 @@ import 'https://esm.sh/@tensorflow/tfjs-core@4.22.0/es2022/dist/register_all_gra
 import 'https://esm.sh/@tensorflow/tfjs-core@4.22.0/es2022/tfjs-core.mjs';
 import 'https://esm.sh/@tensorflow/tfjs-data@4.22.0/es2022/tfjs-data.mjs';
 import 'https://esm.sh/@tensorflow/tfjs-layers@4.22.0/es2022/tfjs-layers.mjs';
+
+export { tf };
