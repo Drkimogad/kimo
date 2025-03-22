ee4edd7f0bcd94cb22e03e4a00d43fbbe1c2efd4
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-22 18:41:21 +0200

    Update proxy-server.js

--- public/proxy-server.js
+++ public/proxy-server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public'), {
 }));
 
 // Update the proxy URL to use the Vercel URL
-const vercelProxyUrl = 'https://kimo-clm2ld8om-drkimogad-s-projects.vercel.app//api/proxy';
+const vercelProxyUrl = 'https://kimo-peach.vercel.app/';
 
 app.get('/api/proxy', async (req, res) => {
   const url = req.query.url;
