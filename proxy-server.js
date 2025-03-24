0eb72a1981201dc82b48a52cd450566c809eaba6
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-24 12:36:49 +0200

    Update proxy-server.js

--- proxy-server.js
+++ proxy-server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 // Configure environment variables
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5173;
 const {
   GOOGLE_API_KEY,
   GOOGLE_CX,
