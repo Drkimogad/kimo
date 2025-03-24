2d939a248e8c99ec0eef007509b288aef67f731a
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-24 02:05:46 +0200

    Create offlineStorage.js

---
+++ utils/offlineStorage.js
@@ -0,0 +1,51 @@
+const DB_NAME = 'kimoAI';
+const DB_VERSION = 2;
+
+export class OfflineStorage {
+  static async init() {
+    if (!this.db) {
+      this.db = await this._openDatabase();
+    }
+    return this.db;
+  }
+
+  static _openDatabase() {
+    return new Promise((resolve, reject) => {
+      const request = indexedDB.open(DB_NAME, DB_VERSION);
+
+      request.onupgradeneeded = (event) => {
+        const db = event.target.result;
+        if (!db.objectStoreNames.contains('searchHistory')) {
+          db.createObjectStore('searchHistory', { keyPath: 'id', autoIncrement: true });
+        }
+      };
+
+      request.onsuccess = () => resolve(request.result);
+      request.onerror = () => reject(request.error);
+    });
+  }
+
+  static async add(storeName, data) {
+    const db = await this.init();
+    return new Promise((resolve, reject) => {
+      const tx = db.transaction(storeName, 'readwrite');
+      const store = tx.objectStore(storeName);
+      const request = store.add({ ...data, id: Date.now() });
+      
+      request.onsuccess = () => resolve();
+      request.onerror = () => reject(request.error);
+    });
+  }
+
+  static async getAll(storeName) {
+    const db = await this.init();
+    return new Promise((resolve, reject) => {
+      const tx = db.transaction(storeName, 'readonly');
+      const store = tx.objectStore(storeName);
+      const request = store.getAll();
+      
+      request.onsuccess = () => resolve(request.result);
+      request.onerror = () => reject(request.error);
+    });
+  }
+}
