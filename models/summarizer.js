4d623efa2d1d332d8b333535967dd5d52a227a29
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-24 21:53:30 +0200

    Update summarizer.js

--- models/summarizer.js
+++ models/summarizer.js
@@ -1,4 +1,4 @@
-import { pipeline } from '../t5-small/pipelines.js';
+import { pipeline } from '@huggingface/transformers';
 const SUMMARY_CACHE = new Map();
 const CLOUD_SUMMARY_URL = import.meta.env.VITE_SUMMARY_API || '/api/summarize';
 
