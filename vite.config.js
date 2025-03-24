f5e8a894e29630223fb6c275454e3e27911c8811
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-24 02:53:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,19 @@
+import { defineConfig } from 'vite';
+
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Your proxy server port
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      '/models': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['@xenova/transformers']
   }
