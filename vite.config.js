db0767f5231123789b4c56ad3dc79eedd1061285
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-26 21:32:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import nodePolyfills from 'vite-plugin-node-polyfills';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [
-    nodePolyfills()
-  ]
+  plugins: [nodePolyfills()],
+  build: {
+    target: 'esnext', // Ensures modern build
+    rollupOptions: {
+      output: {
+        // Prevent dynamic import warnings
+        manualChunks: {
+          onnxruntime: ['onnxruntime-web'], // Optional to reduce bundle size
+        },
+      },
+    },
+  },
 });
